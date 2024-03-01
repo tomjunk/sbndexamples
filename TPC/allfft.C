e2bfc18a310175137c63cacaff64ec5841c9c742
Tom Junk <trj@fnal.gov>
2024-02-29 22:40:07 -0600

    FFT all channels and make one plot per channel

---
+++ TPC/allfft.C
@@ -0,0 +1,99 @@
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "canvas/Utilities/InputTag.h"
+#include "gallery/Event.h"
+
+#include "TFile.h"
+#include "TInterpreter.h"
+#include "TROOT.h"
+#include "TProfile.h"
+#include "TMath.h"
+#include "TVirtualFFT.h"
+#include "lardataobj/RawData/RawDigit.h"
+
+using namespace art;
+using namespace std;
+
+// gallery script to make an FFT plot for every channel in SBND
+
+// arguments:  filename -- input file, larsoft formatted
+// ievcount:  which event to display the FFT for.  This is the tree index in the file and not the event number
+// tickmin, tickmax -- to truncate the part of the event to run the FFT on.  Set to big and small numbers for no truncation.
+// digifreq:  ADC Sampling frequency in MHz.  Used to label plots.  It's 2 MHz for 35t, DUNE FD MC, and 2.5 MHz for 3x1x1
+// inputtag: use "tpcrawdecoder:daq" for input
+
+void allfft(std::string const& filename="tpcdecode_data_evb03_run11395_2_20240227T194156.root",  
+            size_t tickmin=0,
+            size_t tickmax=3413,  // we have an odd number of ticks, 3415; take only the first 3414 
+            std::string const& inputtag="daq", 
+            double digifreq=2.0,
+            TString outputfilename="allchanfft.root")
+{
+  InputTag rawdigit_tag{ inputtag };
+  vector<string> filenames(1, filename);
+
+  std::map<size_t,TProfile*> profmap;
+
+  for (gallery::Event ev(filenames); !ev.atEnd(); ev.next()) {
+    auto const& rawdigits = *ev.getValidHandle<vector<raw::RawDigit>>(rawdigit_tag);
+    if (!rawdigits.empty())
+      {
+        size_t nrawdigits = rawdigits.size();
+
+        size_t tlow = TMath::Max(tickmin, (size_t) 0);
+        size_t thigh = TMath::Min(tickmax, (size_t) rawdigits[0].Samples()-1); // assume uncompressed; all channels have the same number of samples
+        size_t nticks = thigh - tlow + 1;
+
+        double x[nticks];
+        double re=0;
+        double im=0;
+        double mag=0;
+        double sf = digifreq/nticks;
+
+        Int_t nti = nticks;
+        TVirtualFFT *fftr2c = TVirtualFFT::FFT(1,&nti,"R2C ES K");
+        for (size_t ichan=0;ichan<nrawdigits;++ichan)
+          {
+            size_t ic = rawdigits[ichan].Channel(); 
+            if (profmap.find(ic) == profmap.end())
+              {
+                TString hid = "chan";
+                hid += ic;
+                TString htitle = "Channel ";
+                htitle += ic;
+                htitle += ";Frequency (MHz);FFT";
+
+                //std::cout << "creating histo: " << ic << std::endl;
+                profmap[ic] = (TProfile*) new TProfile(hid,htitle,nticks/2,0,digifreq/2.0);
+                profmap[ic]->SetDirectory(0);
+              }
+            TProfile *cf1dt = profmap[ic];
+                
+            for (size_t itick=tlow; itick <= thigh; ++itick) x[itick-tlow] = rawdigits[ichan].ADC(itick); 
+            //cout << x[0] << " " << x[1] << " " << x[2] << endl;
+            fftr2c->SetPoints(x);
+            fftr2c->Transform();
+            for (size_t i=0;i<nticks/2;++i)
+              {
+                fftr2c->GetPointComplex(i, re, im);
+                mag = TMath::Sqrt(re*re + im*im);
+                // cout << mag << endl;
+                cf1dt->Fill(i*sf,mag); 
+              }
+          }
+        //cout << "Finished fft'ing channels" << endl;
+      }  // end check if rawdigits are empty
+  } // end loop over events
+  TFile outputfile(outputfilename,"RECREATE");
+  outputfile.cd();
+  for (auto const& iter : profmap)
+    {
+      iter.second->Write();
+    }
+  std::cout << "closing output file" << std::endl;
+  //outputfile.Close();
+}
+
